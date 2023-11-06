78a77d318f1dfb517e2d590dfcd185d87b2a4da6
Abhishek <117010831+bh-Abhishek-b@users.noreply.github.com>
2023-11-06 16:34:27 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -25,7 +25,7 @@ with st.sidebar:
     st.write('''-[Streamlit](https://streamlit.io/) 
             -  [OpenAI](https://platform.openai.com/docs/models)''')
 @st.cache_resource 
- def init_connection():
+def init_connection():
         return pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
         + st.secrets["SERVER_NAME"]
